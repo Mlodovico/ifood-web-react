60a40f93321d480c26b8d8acc0a5bb2d64219545
mlodovico <murilo.lodovico@outlook.com>
2024-07-08 21:57:38 -0300

    feature: resolving styles modal problem

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     position: absolute;
+    z-index: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
